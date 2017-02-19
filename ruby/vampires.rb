08b8af7cdd67a2f31e942dddce8f0e576faebde3
Rakey Drammeh <rakeydrammeh@Rakeys-MBP.fios-router.home>
2017-02-18 19:01:00 -0500

    Created a vampire interview question form for release 1 for 4.4

---
+++ ruby/vampires.rb
@@ -0,0 +1,11 @@
+puts "What is your name?"
+employee_name = gets.chomp
+
+puts "How old are you? What year were you born? (age, year)"
+age_year = gets.chomp 
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+garlic_bread = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+health_insurance = gets.chomp 
