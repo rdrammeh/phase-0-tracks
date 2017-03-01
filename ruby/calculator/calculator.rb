d12269efb70387d2ebd83c8b23ca73f4edb005b5
Rakey Drammeh <rakeydrammeh@Rakeys-MacBook-Pro.local>
2017-03-01 01:08:54 -0500

    created methods for adding, subtracting and multipling

--- ruby/calculator/calculator.rb
+++ ruby/calculator/calculator.rb
@@ -1,13 +1,17 @@
-class Calculator
-  def add(x,y)
-    x + y
-  end
-
-  def subtract(x,y)
-    x - y
-  end
-
-  def multiply(x,y)
-    x * y
-  end
+def add(x, y)
+  x + y
 end
+def subtract(x, y)
+  x - y
+end
+def multiply(x, y)
+  x * y
+end
+
+#driver code
+#Printing 2 + 3 
+p add(2, 3)
+#Printing 45 - 27
+p subtract(45, 27)
+#Printing 9 * 18
+p multiply(9.18)
