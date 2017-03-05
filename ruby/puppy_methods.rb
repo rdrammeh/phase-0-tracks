2074c42e7a9e0cc0aba863ade36222407f9b2098
Rakey Drammeh <rakeydrammeh@Rakeys-MBP.fios-router.home>
2017-03-05 11:51:54 -0500

    created an exercise class for module 6.2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -35,3 +35,37 @@ instance.speak(3)
 instance.roll_over
 instance.dog_years(24)
 instance.sit
+# ////////// DRIVER CODE //////////
+
+class Exercise 
+
+  def cardio(hours)
+    puts "I have to complete #{hours} hours of cardio this week"
+  end
+
+  def strength(box_jumps)
+    box_jumps.times do puts "box jump!"
+    end
+    puts "I completed #{box_jumps} box jumps this week!"
+  end 
+
+  def initialize 
+    puts "Initializing new Exercise instance..."
+  end
+
+end
+
+# ////////// DRIVER CODE //////////
+instance = Exercise.new
+instance.cardio(7)
+instance.strength(10)
+
+array = []
+  50.times do instance = Exercise.new
+    array.push(instance)
+  end
+
+  array.each do |workout|
+    workout.cardio(7)
+    workout.strength(10)
+  end
