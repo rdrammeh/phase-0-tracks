3e60ebded79bf2fefefb0cf17a4929f5e130d68a
Rakey Drammeh <rakeydrammeh@Rakeys-MBP.fios-router.home>
2017-03-05 10:11:05 -0500

    Created charactertistics for a puppy class for 6.1

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+# SPECIES ------------------------
+# Pupptachious fluffoball
+# 
+# CHARACTERISTICS ----------------
+# Size: varies
+# Friendly: yes
+# Eye count: 2
+# Name: varies
+# Color: varies
+# Legs: 4
+# 
+# BEHAVIOR -----------------------
+# Lick
+# Run
+# Bark 
