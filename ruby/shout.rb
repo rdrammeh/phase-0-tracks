57158cc5b3ceaacf9890f2751fdac4fb47832a0a
Rakey Drammeh <rakeydrammeh@Rakeys-MBP.fios-router.home>
2017-03-05 18:04:26 -0500

    converted a standalone module to a mixin for release 3

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,15 +1,39 @@
 #Release 1: Write a simple Module
 
-module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+# 
+#   def self.yelling_happily(words)
+#     words + "***" + " :D"
+#   end
+# end
+# 
+# # ///////// driver code //////////// #
+# puts Shout.yell_angrily("ugh")
+# puts Shout.yelling_happily("yay")
 
-  def self.yelling_happily(words)
-    words + "***" + " :D"
+#Release 3: Convert a Standalone Module to a Mixin
+module Shout 
+  def pitch(loudness)
+    puts "#{loudness} is the tone volume used at this sporting event."
   end
 end
 
-# ///////// driver code //////////// #
-puts Shout.yell_angrily("ugh")
-puts Shout.yelling_happily("yay")
+class Baseball 
+  include Shout
+end
+
+class Golf
+  include Shout
+end
+
+#///// DRIVER CODE /////#
+baseball = Baseball.new
+baseball.pitch("High")
+
+golf = Golf.new 
+golf.pitch("Low")
+
+
