487f512856079baef9e910987afa675417729a33
Rakey Drammeh <rakeydrammeh@Rakeys-MBP.fios-router.home>
2017-03-05 17:09:39 -0500

    Added refactors and also built more santas through randomization

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,9 @@
 class Santas
 
+#Release 3: refactor with attr_reader and attr_accessor
+  attr_reader :age, :ethnicity
+  attr_accessor :gender, :name
+
 #Release 0: Bring Santa to Life 
   def speak
     puts "Ho, ho, ho! Haaaappy holidays!"  
@@ -16,54 +20,34 @@ class Santas
     @gender = gender 
     @ethnicity = ethnicity
     @jingle = jingle 
-    @age = 0
+    @age = rand(0-140).to_i
     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
   end
 
 # Release 2: Change an attribute with a Method
-  def celebrate_birthday(age)
-    @age = age + 1 
+  def celebrate_birthday
+    @age = @age + 1 
   end
-
 
   def get_mad_at(reindeer_name)
     @reindeer_ranking.delete(reindeer_name)
     @reindeer_ranking[-1] = reindeer_name
-    reindeer_name
   end
-  puts "#{@name}'s reindeer list is: #{@reindeer_ranking}"
-
-  def gender(new_gender)
-    @gender = new_gender
-    @gender
-  end
-  puts "Your new gender is #{@gender}"
-
-  def age 
-    @age
-  end
-
-  def ethnicity 
-    @ethnicity
-  end
-
 end 
 
-#////////DRIVER CODE //////// #
-# santas = Santas.new
-# santas.speak
-# santas.eat_milk_and_cookies("snickerdoodle")
-#////////DRIVER CODE //////// #
-
-#////////DRIVER CODE //////// #
-# santas = Santas.new("female", "black", 24, "Jingle Bell Rock")
-
-#////////DRIVER CODE //////// #
-santas = Santas.new("female", "black", "Jingle Bell")
-santas.celebrate_birthday(5)
-santas.get_mad_at("Rudolph")
-santas.gender("male")
-santas.age
-santas.ethnicity
 
 
+# Release 4: Build Many Santas #
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+jingle = ["Santa Baby", "Jingle Bell", "All I Want for Christmas", "Santa Claus is Coming to Town"]
+
+# //////// DRIVER CODE /////// #
+santas = Santas.new(example_genders.sample, example_ethnicities.sample, jingle.sample)
+santas.speak
+santas.eat_milk_and_cookies("snickerdoodle")
+puts "#{santas.name} is #{santas.age} years old and identifies as #{santas.gender} and #{santas.ethnicity}"
+santas.celebrate_birthday
+puts "#{santas.name}'s updated age is #{santas.age}"
+santas.get_mad_at("Dancer")
+puts "#{santas.name} moved Dancer to the end of the ranking."
