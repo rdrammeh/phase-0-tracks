b14d4e722a2e06d90538c3be63edfde5e5bca72e
Rakey Drammeh <rakeydrammeh@Rakeys-MBP.fios-router.home>
2017-03-05 14:05:58 -0500

    Declared a Santa class and two instances and also initialize the method

---
+++ ruby/santa.rb
@@ -0,0 +1,19 @@
+class Santa
+  
+  def speak
+    puts "Ho, ho, ho! Haaaappy holidays!"  
+  end
+
+  def eat_milk_and_cookies(cookie_type)
+    puts "That was a good #{cookie_type}!"
+  end
+
+  def initialize 
+    puts "Initializing Santa instance"
+  end
+end 
+
+#////////DRIVER CODE ////////
+instance = Santa.new
+instance.speak
+instance.eat_milk_and_cookies("snickerdoodle")
