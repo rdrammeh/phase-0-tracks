f62bbd227b3e7f5bd98fd1eabddf480164885a45
Rakey Drammeh <rakeydrammeh@Rakeys-MBP.fios-router.home>
2017-03-05 11:12:12 -0500

    created puppy methods for module 6.2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,29 @@ class Puppy
     toy
   end
 
+  def speak(bark)
+    bark.times do puts "Woof!"
+    end
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(human_years)
+    puts human_years.to_i * 15 
+  end
+
+  def sit
+    puts "*sit*"
+  end
+
 end
 
+# ////////// DRIVER CODE //////////
+instance = Puppy.new 
+instance.fetch("ball")
+instance.speak(3)
+instance.roll_over
+instance.dog_years(24)
+instance.sit
