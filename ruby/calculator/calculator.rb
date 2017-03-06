40c083aaefd9dc5515080916da1db43efb74aea3
Rakey Drammeh <rakeydrammeh@Rakeys-MBP.fios-router.home>
2017-03-05 19:53:47 -0500

    Wrote a 'divide' test to ensure that it passed

--- ruby/calculator/calculator.rb
+++ ruby/calculator/calculator.rb
@@ -1,17 +1,17 @@
-def add(x, y)
-  x + y
+class Calculator
+  def add(x, y)
+    x + y
+  end
+  
+  def subtract(x, y)
+    x - y
+  end
+  
+  def multiply(x, y)
+    x * y
+  end
+#Release 1: Write a RSpec test
+  def divide(x,y)
+    x / y
+  end
 end
-def subtract(x, y)
-  x - y
-end
-def multiply(x, y)
-  x * y
-end
-
-#driver code
-#Printing 2 + 3 
-p add(2, 3)
-#Printing 45 - 27
-p subtract(45, 27)
-#Printing 9 * 18
-p multiply(9.18)
