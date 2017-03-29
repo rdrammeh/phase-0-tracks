d06c986c68e854275392bb908090d9594df4b595
Rakey Drammeh <rakeydrammeh@Rakeys-MBP.fios-router.home>
2017-03-28 23:20:05 -0400

    Added comments and fixed loops issues. This was completed in repl which is why there aren't many commits.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,69 +1,85 @@
-def werewolf_inc_member_form
-  puts "How many employees will you be processing?"
-  number = gets.chomp
+## Werewolf Inc Interview Form (Release 1) ##
 
+# Processing Multiple Employees (Release 3) #
+puts "How many employees are you processing?"
+employees = gets.chomp.to_i
+  
+until employees == 0
   puts "What is your name?"
-  employee_name = gets.chomp
-
+  name = gets.chomp
+  
   puts "How old are you?"
   age = gets.chomp.to_i
-
+  
   puts "What year were you born?"
   year_born = gets.chomp.to_i
-    if year_born + age == 2017  
-      puts "Year is equal to age"
-      year_born = true  
-    else
-      puts "Year NOT equal to age"
-      year_born = false
-    end
-
-  puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
-  garlic_bread = gets.chomp
-    if garlic_bread == "y"
-      puts "Likes garlic bread"
-      garlic_bread = true
-    else
-      puts "Does NOT like garlic bread"
-      garlic_bread = false
-    end
-
-  puts "Would you like to enroll in the company's health insurance?"
+  
+  puts "Would you like garlic bread at lunch? (yes/no)"
+  garlic_bread = gets.chomp 
+  
+  puts "Want to enroll for health insurance? (yes/no)"
   health_insurance = gets.chomp 
-    if health_insurance == "y"
-      puts "Wants health insurance"
-      health_insurance = true
-    else
-      puts "Does NOT want health insurance"
-      health_insurance = false
-    end
+  
+# Check for suspicious allergies(release 4) #
+allergies = nil 
 
-  puts "Do you have any allergies (describe)?"
-  allergies = gets.chomp
-    until allergies == "done"
-      puts "What other allergies?"
-      allergies = "#{allergies}"
+  puts "Name any allergies one at a time. If none type 'done'"
+  allergies = gets.chomp 
+    until allergies == "done" || allergies == "sunshine"
+      puts "Any more?"
+      allergies = gets.chomp
     end
-    if allergies == "Sunshine"
-      puts "Probably a vampire"
+    if allergies == "sunshine"
+      allergic_to_sunshine = true
     end
-end
+  
 
-year_born = true
-garlic_bread = true
-health_insurance = true
+## Conditional Statements (Release 2) ## 
 
-puts werewolf_inc_member_form
-  if year_born && (garlic_bread || health_insurance) 
-      puts "Probably NOT a vampire"
-  elsif !year_born && !(garlic_bread || health_insurance)
-      puts "Probably a vampire"
-  elsif !(year_born && garlic_bread && health_insurance)
-      puts "Almost certainly a vampire"
-  elsif employee_name == "Drake Cula" || "Tu Fang"
-      puts "Definitely a vampire"
+# age #
+  if 2017 - age == year_born
+    puts "Age is equal to year born"
+    age = true 
   else 
-      puts "Results inconclusive"
+    puts "Age is NOT equal to year born"
+    age = false 
   end
 
-  puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
+# garlic bread #
+  if garlic_bread == "yes"
+    puts "Wants garlic bread"
+    garlic_bread = true 
+  else 
+    puts "Does NOT want garlic bread"
+    garlic_bread = false
+  end 
+
+# health insurance # 
+  if health_insurance == "yes"
+    puts "Wants health insurance"
+    health_insurance = true 
+  else
+    puts "Does NOT want health insurance"
+    health_insurance = false 
+  end 
+
+# Interview Results #
+  if age == true && (garlic_bread == true || health_insurance == true)
+    puts "Probably NOT a vampire"
+  elsif age == false && (garlic_bread == false || health_insurance == false)
+    puts "Probably a vampire"
+  elsif allergic_to_sunshine == true
+    puts "Probably a vampire"
+  elsif age == false && garlic_bread == false && health_insurance == false
+    puts "Almost certainly a vampire"
+  elsif name == "Drake Cula" || name == "Tu Fang"
+    puts "Definitely a vampire"
+  else
+    puts "Results inconclusive"
+  end
+
+employees = employees - 1 
+end 
+
+# Plot Twist (release 5) #
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
