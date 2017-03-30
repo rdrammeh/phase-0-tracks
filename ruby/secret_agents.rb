de4e898a891a7a5384d03e913d8ead9b10859d2f
Rakey Drammeh <rakeydrammeh@Rakeys-MBP.fios-router.home>
2017-03-29 22:06:44 -0400

    Completed release 0 of 4.6

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,60 +1,38 @@
-# - ask for a password
-# - get the password
-# - user enters only letters. no numbers or symbols 
-# - every letter that a user puts in, the computer will 
-#   choose the next corresponding letter in 
-#   the alphabet i.e. 'a' --> 'b'
+# Build and Encryption Program (Release 2)#
 
-def encrypt(password)
-  encrypted_password = ""
+# encrypt #
+# Assign the variable index to 0  
+# Assign "abc" to the variable str 
+# Create a while loop to cycle through the letters in the str
+# Set str index so that the next letter in the str returns 
+# Increment loop by 1 
+# Print str 
 
 
-   index = 0
-  while index < password.length
-      if password[index] == "z"
-      password[index] = "a"
-      encrypted_password = encrypted_password + password[index]
-  
-    index += 1
-    else
+# index = 0
+# str = "abc"
+# 
+# while index < str.length
+#   str[index] = str[index].next
+#   index += 1
+# end
+# 
+# p str
+# 
 
-    encrypted_password = encrypted_password + password[index].next
-    index += 1
-    end
-  end
+# decrypt #
+# Set index to 0 
+# Assign a string "bcd" to a variable
+# Find the index of the string 
+# Subtract the index of that current from 1 
+# Print that new string 
 
-   p encrypted_password
+index = 0
+str = "bcd"
+
+while index < str.length
+  str[index] = str[index] - 1
+  index -= 1
 end
 
-puts encrypt("abc")
-puts encrypt("zed")
-puts encrypt("bcd")
-puts encrypt("afe")
-
-
-# - ask for a password 
-# - get the password 
-# - computer will turn the letters back to go back on. 
-
-def decrypt(password)
-  decrypted_password = ""
- index = 0
-while index < password.length
-    if password[index] == "z"
-    password[index] = "a"
-    decrypted_password = decrypted_password + password[index]
-    
-
-  index += 1
-  else
-  decrypted_password = decrypted_password + password[index].next
-  index += 1
-  
-  reversed_password = decrypted_password.reverse
-  end
-end
-
-p reversed_password
-end
-
-puts decrypt("bcd")
+p str
