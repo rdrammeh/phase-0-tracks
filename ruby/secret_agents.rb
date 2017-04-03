dd9736986715e14b5190da2660d71fceaaa1f4dd
Rakey Drammeh <rakeydrammeh@Rakeys-MBP.fios-router.home>
2017-04-03 00:26:55 -0400

    Added driver code and printed results to the screen

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -45,11 +45,36 @@ return str
 end 
 
 
-encrypt("abc") 
-encrypt("zed") 
-decrypt("bcd") 
-decrypt("afe")
-decrypt(encrypt("swordfish"))
+# encrypt("abc") 
+# encrypt("zed") 
+# decrypt("bcd") 
+# decrypt("afe")
+# decrypt(encrypt("swordfish"))
 
 ### Nested Method ###
 #This nested method call works because the argument we gave is within the parameters of both the encrypt and decrypt method
+
+## Release 5: Add an Interface ##
+
+## DRIVER CODE ## 
+
+# ask the agent if they want to encrypt or decrypt a password"
+puts "Secret Agent: Do you want to decrypt or encrypt a password?"
+answer = gets.chomp 
+  until answer == "decrypt" || answer == "encrypt"
+    puts "I'm sorry I don't understand. Please say 'encrypt' or 'decrypt"
+    answer = gets.chomp
+  end
+puts "Secret Agent wants to #{answer} a password."
+
+# ask agent for the password and print it
+puts "What is the password?"
+password = gets.chomp
+puts "You want to #{answer} this password: #{password}."
+
+#print new password
+if answer == "encrypt"
+  puts encrypt("#{password}")
+else 
+  puts decrypt("#{password}")
+end
