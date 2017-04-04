64388992d7ff9f0fe7e6979bbd6afc15efca5ee0
Rakey Drammeh <rakeydrammeh@Rakeys-MacBook-Pro.local>
2017-04-04 16:56:22 -0400

    Iterated on each array and hash using, each, map and map! for 5.3 module.

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -15,3 +15,51 @@ def status_message
 end
 
 status_message { |name1, name2| puts "#{name1} and #{name2} are working on this pairing session together!" }
+
+# RELEASE 1 #
+grocery_list = [
+  "eggs", 
+  "milk", 
+  "bread", 
+  "cereal", 
+  ]
+ 
+car_types = {
+  Ford: "Explorer",
+  Honda: "Accord",
+  Tesla: "S",
+  Volkswagon: "Beetle"
+ } 
+
+ # Iterating on an array with .each 
+puts "Original grocery list:" 
+p grocery_list
+
+grocery_list.each do |item|
+  puts "We are in need of #{item}."
+end
+
+# Iterating on a hash with .each 
+puts "Original car list:"
+p car_types
+
+car_types.each do |brand, model|
+  puts "The #{model} model is a brand of #{brand}."
+end
+
+# Iterating on an array with .map
+puts "Original grocery list:"
+p grocery_list
+
+fake_list = grocery_list.map do |item|
+  puts "fake #{item}"
+end
+
+# Iterating on an array with .map!
+puts "Original grocery list"
+p grocery_list
+
+grocery_list.map! do |item|
+  grocery_list = "Real #{item}"
+  p grocery_list
+end
