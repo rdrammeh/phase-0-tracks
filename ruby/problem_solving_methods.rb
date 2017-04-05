1a827569b06ad8a33f6263fd1ac9cb4ade5ec3c6
Rakey Drammeh <rakeydrammeh@Rakeys-MacBook-Pro.local>
2017-04-05 15:09:21 -0400

    Created a method to calculate Fibonacci numbers.

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,21 +1,36 @@
 ### RELEASE 0 ###
 
-numbers = [5, 17, 23, 36, 42, 58, 66, 78, 80]
+# numbers = [5, 17, 23, 36, 42, 58, 66, 78, 80]
+# 
+# def search_array(arr, x)
+#   numbers = arr
+#   index = 0 
+#   while index < numbers.length
+#     numbers.each do |search|
+#       if search == x
+#         p index
+#       end
+#       index += 1
+#     end
+#   end
+# end
+# 
+# search_array(numbers, 36)
 
-def search_array(arr, x)
-  numbers = arr
-  index = 0 
-  while index < numbers.length
-    numbers.each do |search|
-      if search == x
-        p index
-      end
-      index += 1
-    end
+### RELEASE 1 ### 
+
+def fib(count)
+  arr = [0, 1]
+  count.times do
+    arr << arr[-1] + arr[-2]
   end
+  arr.pop
+  arr.pop
+  p arr[-1]
 end
 
-search_array(numbers, 36)
+p fib(100)
+
 
 
 
