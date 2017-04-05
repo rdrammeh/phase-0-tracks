fe7d237e19dbf3176b07cfc7d0d2d1a4f21c452e
Rakey Drammeh <rakeydrammeh@Rakeys-MacBook-Pro.local>
2017-04-05 14:34:01 -0400

    Initial commit

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,13 +1,70 @@
-def good_years (arr, x)
-  arr = [2010, 2012, 2015, 2017]
+### RELEASE 0 ###
+
+numbers = [5, 17, 23, 36, 42, 58, 66, 78, 80]
+
+def search_array(arr, x)
+  numbers = [5, 17, 23, 36, 42, 58, 66, 78, 80]
+  index = 0 
+  while index < numbers.length
+    numbers.each do |search|
+      if search == x
+        p index
+      end
+      index += 1
+    end
+  end
 end
 
-p good_years(arr, 2012)
+search_array(numbers, 36)
 
 
-def fib(number)
-  arr = [0, 1, 1, 2, 3, 5, "number"]
-  arr.next = arr.last + arr.index[-2]
-end
 
-fib(7)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def good_years (arr, x)
+#   arr = [2010, 2012, 2015, 2017]
+# end
+# 
+# p good_years(arr, 2012)
+# 
+# 
+# def fib(number)
+#   arr = [0, 1, 1, 2, 3, 5, "number"]
+#   arr.next = arr.last + arr.index[-2]
+# end
+# 
+# fib(7)
