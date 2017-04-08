5a35d66e464c6be443b74c52610f24f0ee29ec39
Rakey Drammeh <rakeydrammeh@Rakeys-MBP.fios-router.home>
2017-04-08 18:38:06 -0400

    Initial commit. Created a nested data structure for a restaurant menu

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,22 +1,77 @@
-nyc_travel_plan = {
-  hotels: [
-    "Hilton Garden Inn",
-    "Wyndham Hotel", 
-    "Waldorf Astoria" 
-  ],
-  restaurants: [
-    "Jackson Hole",
-    "Agave",
-    "Refinery"
-  ],
-  activities: [
-    "Statue of Liberty",
-    "Ellis Island",
-    "Freedom Tower"
-  ]
+#--- 5.4 Nested Data Structures ---#
+
+restaurant_menu = {
+  entre: {
+    vegetarian: {
+      salad: [
+        'Kale',
+        'Caesar',
+        'Nicoise'
+      ],
+      soup: [
+        'Lentil',
+        'Black Bean & Salsa',
+        'Butternut Squash'
+      ]
+    },
+    meat: {
+      wings: [
+        'BBQ',
+        'Buffalo'
+      ],
+      sliders: [
+        'Beef',
+        'Chicken',
+        'Turkey'
+      ],
+    }
+  },
+  dessert: {
+    cake: [
+      'Red Velvet',
+      'Carrot',
+      'Chessecake'
+    ],
+    ice_cream: [
+      'Cookies & Cream',
+      'Rocky Road',
+      'Butter Pecan'
+    ]
+  },
+  drinks: {
+    alcohol: {
+      red_wine: [
+        'Malbec',
+        'Pinot Noir',
+        'Merlot'
+      ],
+      white_wine: [
+        'Pinot Grigio',
+        'Moscato',
+        'Chardonnay'
+      ]
+    },
+    soft_drinks: [
+      'Coca-Cola',
+      'Fanta',
+      'Ginger-ale'
+    ],
+    caffine: {
+      tea: [
+        'English Breakfast',
+        'Earl Grey',
+        'Lemon'
+      ],
+      coffee: [
+        'Hazlenut',
+        'Coconut Mocha'
+      ]
+    }
+  }
 }
 
-nyc_travel_plan[:hotels][2]
-nyc_travel_plan[:restaurants][0]
-nyc_travel_plan[:activities].push("Empire State Building")
-nyc_travel_plan[:hotels][1] = "Top of the Standard"
+#call an item in a list 
+#call another item on a list 
+#add an item to a list 
+#change an item 
+
