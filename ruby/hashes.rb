cfc53003f52498143c4335c7301b26681404007c
Rakey Drammeh <rakeydrammeh@Rakeys-MBP.fios-router.home>
2017-04-08 15:38:22 -0400

    Initial commit with pseudocode for hash program for module 5.2

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,52 +1,12 @@
-#Ask the user their name, location, age, 
-#number of children, decor theme, if they have allergies,
-#and what they hate the most 
-#Create a loop for the last two 
-#Create a hash for the users inputs 
-#If the user needs to make changes, allow them to do so!
-#Print the hash 
+## 5.2 Arrays and Hashes ##
 
-
-puts "Clients name:"
-client_name = gets.chomp 
-
-puts "Location:"
-client_location = gets.chomp 
-
-puts "Age:"
-age = gets.chomp.to_i 
-
-puts "Number of children:"
-children = gets.chomp.to_i
-
-puts "Decor theme:"
-theme = gets.chomp 
-
-loop do 
-  puts "Name your allergies (if not put 'done'):"
-  allergies = gets.chomp 
-  break if allergies == 'done'
-end
-
-loop do 
-  puts "What do you loathe the most (if nothing put 'done'):"
-  loathe = gets.chomp 
-  break if loathe == 'done'
-end
-
-interior_designers = {
-  "Name" => "#{client_name}",
-  "Location" => "#{client_location}",
-  "Age" => "#{age}",
-  "Children" => "#{children}",
-  "Theme" => "#{theme}",
-  "Allergies" => "+=#{allergies}",
-  "loathe" => "+=#{loathe}"
-}
-
-puts interior_designers
-
-
-
+#--- Pseudocode Hash Program for Interior Designer ---# 
+#Ask designer if they for client name, age, number of children, decor theme, and what they loathe
+#Store responses in variables and convert user format to appropriate type 
+#Create a hash with the designer's inputs 
+#Print the hash to the screen to ensure everything was captured well 
+#Ask the user if they want to update anything using the appropriate key and value tags 
+#Use control flow to print the hash if their are no changes and to prompt user for more info if their are changes 
+#Print the final hash
 
 
