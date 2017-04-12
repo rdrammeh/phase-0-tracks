df8d49114230f91d8d057dfc1a5163964b00e5e5
Rakey Drammeh <rakeydrammeh@Rakeys-MBP.fios-router.home>
2017-04-12 17:26:40 -0400

    Initial commit. Created a Puppy class and 5 instance methods and also an initialize method.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,3 +1,4 @@
+#--- RELEASE 0: INSTANCE METHODS ---#
 class Puppy
 
   def fetch(toy)
@@ -15,7 +16,7 @@ class Puppy
   end
 
   def dog_years(human_years)
-    puts human_years.to_i * 15 
+    puts "I am #{human_years.to_i / 15} in dog years."
   end
 
   def sit
@@ -23,49 +24,51 @@ class Puppy
   end
 
   def initialize
-    puts "Initializing new puppy instance..."
+    puts "Initializing new puppy..."
   end
 
 end
 
-# ////////// DRIVER CODE //////////
-instance = Puppy.new 
-instance.fetch("ball")
-instance.speak(3)
-instance.roll_over
-instance.dog_years(24)
-instance.sit
-# ////////// DRIVER CODE //////////
-
-class Exercise 
-
-  def cardio(hours)
-    puts "I have to complete #{hours} hours of cardio this week"
-  end
-
-  def strength(box_jumps)
-    box_jumps.times do puts "box jump!"
-    end
-    puts "I completed #{box_jumps} box jumps this week!"
-  end 
-
-  def initialize 
-    puts "Initializing new Exercise instance..."
-  end
-
-end
+# ////////// DRIVER CODE FOR RELEASE 0 //////////
+shiloh = Puppy.new 
+shiloh.fetch("ball")
+shiloh.speak(3)
+shiloh.roll_over
+shiloh.dog_years(60)
+shiloh.sit
 
 # ////////// DRIVER CODE //////////
-instance = Exercise.new
-instance.cardio(7)
-instance.strength(10)
-
-array = []
-  50.times do instance = Exercise.new
-    array.push(instance)
-  end
-
-  array.each do |workout|
-    workout.cardio(7)
-    workout.strength(10)
-  end
+# 
+# class Exercise 
+# 
+#   def cardio(hours)
+#     puts "I have to complete #{hours} hours of cardio this week"
+#   end
+# 
+#   def strength(box_jumps)
+#     box_jumps.times do puts "box jump!"
+#     end
+#     puts "I completed #{box_jumps} box jumps this week!"
+#   end 
+# 
+#   def initialize 
+#     puts "Initializing new Exercise instance..."
+#   end
+# 
+# end
+# 
+# # ////////// DRIVER CODE //////////
+# instance = Exercise.new
+# instance.cardio(7)
+# instance.strength(10)
+# 
+# array = []
+#   50.times do instance = Exercise.new
+#     array.push(instance)
+#   end
+# 
+#   array.each do |workout|
+#     workout.creatfcardio(7)
+#     workout.strength(10)
+#   end
+# 
