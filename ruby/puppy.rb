48446720ce84d3c4ea489be6d7ea5ec0cb1b3f90
Rakey Drammeh <rakeydrammeh@Rakeys-MBP.fios-router.home>
2017-04-12 15:54:30 -0400

    Initial commit. Added characteristics and behavior for a Puppy class.

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -1,15 +1,19 @@
-# SPECIES ------------------------
-# Pupptachious fluffoball
-# 
-# CHARACTERISTICS ----------------
-# Size: varies
-# Friendly: yes
-# Eye count: 2
-# Name: varies
-# Color: varies
-# Legs: 4
-# 
-# BEHAVIOR -----------------------
-# Lick
-# Run
-# Bark 
+#-- RESLEASE 0: DESIGN A CLASS --#
+
+=begin
+SPECIES ------------------------
+Canis (canines)
+
+CHARACTERISTICS ----------------
+Size: varies
+Friendly: yes
+Eye count: 2
+Name: varies
+Color: varies
+Legs: 4
+
+BEHAVIOR -----------------------
+Lick
+Run
+Bark 
+=end
