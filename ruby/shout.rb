3d72a7f448745f045f7560aad3f394eed0a440be
Rakey Drammeh <rakeydrammeh@Rakeys-MBP.fios-router.home>
2017-04-16 00:30:16 -0400

    inital commit. wrote a simple module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,39 +1,94 @@
-#Release 1: Write a simple Module
+#-- RELEASE 1: WRITE A SIMPLE MODULE --#
+module Shout
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
 
-# module Shout
-#   def self.yell_angrily(words)
-#     words + "!!!" + " :("
-#   end
-# 
-#   def self.yelling_happily(words)
-#     words + "***" + " :D"
-#   end
-# end
-# 
-# # ///////// driver code //////////// #
-# puts Shout.yell_angrily("ugh")
-# puts Shout.yelling_happily("yay")
-
-#Release 3: Convert a Standalone Module to a Mixin
-module Shout 
-  def pitch(loudness)
-    puts "#{loudness} is the tone volume used at this sporting event."
+  def self.yelling_happily(words)
+    words + "*****" + " :D"
   end
 end
 
-class Baseball 
-  include Shout
-end
+puts Shout.yell_angrily("Ugh Monday")
+puts Shout.yelling_happily("It's Friday")
 
-class Golf
-  include Shout
-end
 
-#///// DRIVER CODE /////#
-baseball = Baseball.new
-baseball.pitch("High")
 
-golf = Golf.new 
-golf.pitch("Low")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##Release 1: Write a simple Module
+#
+## module Shout
+##   def self.yell_angrily(words)
+##     words + "!!!" + " :("
+##   end
+## 
+##   def self.yelling_happily(words)
+##     words + "***" + " :D"
+##   end
+## end
+## 
+## # ///////// driver code //////////// #
+## puts Shout.yell_angrily("ugh")
+## puts Shout.yelling_happily("yay")
+#
+##Release 3: Convert a Standalone Module to a Mixin
+#module Shout 
+#  def pitch(loudness)
+#    puts "#{loudness} is the tone volume used at this sporting event."
+#  end
+#end
+#
+#class Baseball 
+#  include Shout
+#end
+#
+#class Golf
+#  include Shout
+#end
+#
+##///// DRIVER CODE /////#
+#baseball = Baseball.new
+#baseball.pitch("High")
+#
+#golf = Golf.new 
+#golf.pitch("Low")
 
 
