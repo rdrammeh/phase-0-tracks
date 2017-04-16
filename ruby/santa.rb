04c2b81819c729f0cb71edb45e7066ca17f71901
Rakey Drammeh <rakeydrammeh@Rakeys-MBP.fios-router.home>
2017-04-15 22:46:55 -0400

    Added 5 additional methods that changed scope of some of the attributes

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -27,7 +27,47 @@
     @reindeer_ranking
   end
   
- 
+  # def gender=(new_gender)
+  #   @gender = new_gender
+  # end
+  # 
+  # def age
+  #   @age
+  # end
+  # 
+  # def ethnicity
+  #   @ethnicity
+  # end
+    
+
+end
+
+#-- RELEASE 0 - DRIVER CODE --#
+# rakey = Santa.new 
+# rakey.speak
+# rakey.eat_milk_and_cookies("white chocolate macadamia")
+
+
+# santas = []
+# gender_examples = ["agender", "bigender", "female", "gender fluid", "male", "N/A"]
+# ethnicity_examples = ["black", "Latino", "white", "Japanese", "prefer not tsay""mystical creature (unicorn)", "N/A"]
+# age_examples = [23, 65, 73, 11, 2, 45]
+
+# gender_examples.length.times do |i|
+  # santas << Santa.new(gender_examples[i], ethnicity_examples[i], age_examples[i])
+ # puts "This santa identifies as #{gender_examples[i]} and #{ethnicity_examples[i]}. I am #{age_examples[i]} years old"
+# end
+
+
+#testing get mad at method
+# rakey = Santa.new("female", "black", 24)
+# rakey.get_mad_at("Vixen")
+
+#testing getter methods 
+# santas.length.times do |i|
+  # puts santas[i].age
+  # puts santas[i].ethnicity
+# end
 
 
 
