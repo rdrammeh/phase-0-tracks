d29d7ecdc5a6500c7b3a1269317646c67c11d257
Rakey Drammeh <rakeydrammeh@Rakeys-MBP.fios-router.home>
2017-04-17 00:12:53 -0400

    Re-arranged logic to ensure that letters in word were added to an empty array.

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -1,57 +1,63 @@
-# ///Pseudocode /////// # 
-# create a class for the game
-# define methods for initialize and player 2
-# separate characters of the secret word and replace them with dashes 
-# ask player 2 to guess a letter 
-# their number of guesses is 1 - the length of the word 
-# if they guess the right word, write you got it right 
-# if not, tell them to keep guessing until they do 
+=begin
+#-- GUESS THE WORD --#
+1. Create a class for this game 
+2. Create an initialize method that sets the name of two players and the number of guesses 
+3. Create a method (secret_word)
+- takes word from player 1 and turns the word into spaces disguising the word 
+4. Create a method (guessing_letters)
+- that takes guesses from player 2 one letter at a time 
+- if letter is there, replace empty space with letter 
+- if letter isn't there, say no and ask for another guess 
+- player 2 only has as many guesses as the length of the secret word - 1
+5. Outside of the class create conditional logic that takes all of these pieces to play the game. 
+=end
 
+class Game 
+  attr_reader :word, :winner, :guesses, :secret_word_arr
 
-class GuessTheWord
-
-  attr_accessor :secret_word, :clue
-
-  def initialize(secret_word)
-    @secret_word = secret_word.split('')
-    @guess_progess = "_" * @secret_word.length
-    @guesses = []
-    @guess_count = 0
+  def initialize
+    @winner = false
   end
 
-  def guesses
-    guess = @secret_word.length
-      until guess == 0 
-        puts "Guess a letter"
-        letter = gets.chomp
-          if guesses == 1
-            puts "Last guess"
-          end
-          guess -= 1
-      end 
-  end
-
-  def current_guess(letter)
-    if @guesses.inclue?(letter)
-      puts "You've already guessed this"
-    else 
-      @guesses << letter 
-      @guesses += 1 
+  def secret_word(word)
+    @secret_word_arr = word.split(//)
+    @secret_word_arr.map! do |letter|
+      letter = "_"
     end
   end
 
-end
-
-puts "Welcome to the GuessTheWord Game!"
-game = GuessTheWord.new("apple")
-
-puts "Guess The Word!"
-guess = gets.chomp 
-  if guess == @secret_word
-      puts "WOWZA! You guessed the word!"  
-  else 
-      puts "Keep guessing! You have #{@guesses} left. Guess the word"
-      guess = gets.chomp 
+  def guesses 
+    @guesses = @word.length + 2
   end
 
+  def guess_word(letter)
+    @guesses += 1
+    if @word.include?(letter) 
+      @secret_word.delete("#{letter}")
+      #@secret_word.gsub(/[@word.index(letter)]/, ' _ ' => letter)
+    end
+  end 
+
+end
+
+#-- Guess That Word! --#
+puts "Let's play Guess That Word!"
+game = Game.new 
+
+puts "Player 1:" 
+puts "Give us a word..."
+game.secret_word("lexicon")
+puts "______________________________"
+
+#Making space so that player 2 doesn't see the word.
+50.times do 
+  puts " "
+end
+
+puts "Player 2:"
+puts "Your word has #{game.word.length} letters, therefore you have #{game.guesses} guesses for this game."
+puts game.secret_word("lexicon")
+
+puts "Guess a letter in this word:"
+puts game.guess_word("x")
 
