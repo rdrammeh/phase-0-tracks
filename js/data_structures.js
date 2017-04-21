6349e687590d8a032eaad345df26d85058b2b638
Rakey Drammeh <rakeydrammeh@Rakeys-MBP.fios-router.home>
2017-04-21 12:39:28 -0400

    Built a car object unsing a constructor

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,10 +1,10 @@
 // RELEASE 0: WORK WITH ARRAYS //
-var colors = ["blue", "red", "green", "yellow"]
-var names = ["Skippy", "Slappy", "Stinky", "Mule"]
-
-colors.push("pink");
-names.push("Strinky");
-
+//var colors = ["blue", "red", "green", "yellow"]
+//var names = ["Skippy", "Slappy", "Stinky", "Mule"//]
+//
+//colors.push("pink");
+//names.push("Strinky");
+//
 
 //function newObject {
 //  return {};
@@ -17,16 +17,32 @@ names.push("Strinky");
 //
 //return newObject();
 
-function Car(model, type, year) {
+function Car(model, type, IsGoodCar) {
   console.log("Our car is:", this);
   this.model = model;
   this.type = type;
-  this.year = year;
+  this.IsGoodCar = IsGoodCar;
 
   this.start = function() { console.log("Engine roar!"); };
 
+  this.brake = function() { console.log("Brakes are good!"); };
+
   console.log("CAR INITIALIZATION COMPLETE");
 }
+
+console.log("Car details are:")
+var oneCar = new Car("Ford", "Explorer", true);
+console.log(oneCar);
+console.log("Our car can start:");
+oneCar.start();
+console.log("------")
+
+console.log("Car details are:")
+var secondCar = new Car("Tesla", "S", false);
+console.log(secondCar);
+console.log("Our car can brake:");
+oneCar.brake();
+console.log("------")
 
 
 
