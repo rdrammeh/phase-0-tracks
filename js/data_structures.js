56928e30108a8957f3dfcae783312abca666b256
Rakey Drammeh <rakeydrammeh@Rakeys-MBP.fios-router.home>
2017-04-21 12:49:03 -0400

    Added release comments to code

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,48 +1,51 @@
 // RELEASE 0: WORK WITH ARRAYS //
-//var colors = ["blue", "red", "green", "yellow"]
-//var names = ["Skippy", "Slappy", "Stinky", "Mule"//]
-//
-//colors.push("pink");
-//names.push("Strinky");
-//
+var colors = ["blue", "red", "green", "yellow"]
+var names = ["Skippy", "Slappy", "Stinky", "Mule"]
 
-//function newObject {
-//  return {};
-//}
-//
-//for (i = 0; i < names.length; i++) {
-//  console.log("This horses name is" + " " + names//[i] + "," + "and has a color of" + " " + //colors[i] + ".");
-//  newObject(names[i]) = colors[i];
-//}
-//
-//return newObject();
+colors.push("pink");
+names.push("Strinky");
 
-function Car(model, type, IsGoodCar) {
-  console.log("Our car is:", this);
-  this.model = model;
-  this.type = type;
-  this.IsGoodCar = IsGoodCar;
+// RELEASE 1: BUILD AN OBJECT //
 
-  this.start = function() { console.log("Engine roar!"); };
-
-  this.brake = function() { console.log("Brakes are good!"); };
-
-  console.log("CAR INITIALIZATION COMPLETE");
+function newObject {
+  return {};
 }
 
-console.log("Car details are:")
-var oneCar = new Car("Ford", "Explorer", true);
-console.log(oneCar);
-console.log("Our car can start:");
-oneCar.start();
-console.log("------")
+for (i = 0; i < names.length; i++) {
+  console.log("This horses name is" + " " + names[i] + "," + "and has a color of" + " " + colors[i] + ".");
+  newObject(names[i]) = colors[i];
+}
 
-console.log("Car details are:")
-var secondCar = new Car("Tesla", "S", false);
-console.log(secondCar);
-console.log("Our car can brake:");
-oneCar.brake();
-console.log("------")
+return newObject();
+
+// RELEASE 2: BUILD MANY OBJECTS USING A CONSTRUCTOR //
+
+//function Car(model, type, IsGoodCar) {
+//  console.log("Our car is:", this);
+//  this.model = model;
+//  this.type = type;
+//  this.IsGoodCar = IsGoodCar;
+//
+//  this.start = function() { console.log("Engine //roar!"); };
+//
+//  this.brake = function() { console.log("Brakes //are good!"); };
+//
+//  console.log("CAR INITIALIZATION COMPLETE");
+//}
+//
+//console.log("Car details are:")
+//var oneCar = new Car("Ford", "Explorer", true);
+//console.log(oneCar);
+//console.log("Our car can start:");
+//oneCar.start();
+//console.log("------")
+//
+//console.log("Car details are:")
+//var secondCar = new Car("Tesla", "S", false);
+//console.log(secondCar);
+//console.log("Our car can brake:");
+//oneCar.brake();
+//console.log("------")
 
 
 
