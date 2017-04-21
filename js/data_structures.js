ed8320aa31b943a6301af5730bd7098228a10c50
Rakey Drammeh <rakeydrammeh@Rakeys-MBP.fios-router.home>
2017-04-21 11:11:54 -0400

    Initial commit. Created two arrays and added code that adds items to the arrays.

---
+++ js/data_structures.js
@@ -0,0 +1,6 @@
+// RELEASE 0: WORK WITH ARRAYS //
+var colors = ["blue", "red", "green", "yellow"]
+var names = ["Skippy", "Slappy", "Stinky", "Mule"]
+
+colors.push("pink");
+names.push("Strinky");
