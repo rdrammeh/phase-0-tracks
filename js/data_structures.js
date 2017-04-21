b9a9278cd4a3e63e39cab8ba9c47087cf5d6b829
Rakey Drammeh <rakeydrammeh@Rakeys-MBP.fios-router.home>
2017-04-21 12:28:13 -0400

    Created a car function with one behavior property

--- js/data_structures.js
+++ js/data_structures.js
@@ -6,15 +6,27 @@ colors.push("pink");
 names.push("Strinky");
 
 
-function newObject {
-  return {};
+//function newObject {
+//  return {};
+//}
+//
+//for (i = 0; i < names.length; i++) {
+//  console.log("This horses name is" + " " + names//[i] + "," + "and has a color of" + " " + //colors[i] + ".");
+//  newObject(names[i]) = colors[i];
+//}
+//
+//return newObject();
+
+function Car(model, type, year) {
+  console.log("Our car is:", this);
+  this.model = model;
+  this.type = type;
+  this.year = year;
+
+  this.start = function() { console.log("Engine roar!"); };
+
+  console.log("CAR INITIALIZATION COMPLETE");
 }
 
-for (i = 0; i < names.length; i++) {
-  console.log("This horses name is" + " " + names[i] + "," + "and has a color of" + " " + colors[i] + ".");
-  newObject(names[i]) = colors[i];
-}
-
-return newObject();
 
 
