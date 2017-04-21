c41d74f53286e3864194c20d699b5e21ace0a6fc
Rakey Drammeh <rakeydrammeh@Rakeys-MBP.fios-router.home>
2017-04-21 00:08:33 -0400

    Pseudocoded the steps needed to take to create a reverse a word method

---
+++ js/explore.js
@@ -0,0 +1,12 @@
+// PSEUDOCODE // 
+// 1. Separate the word by letter and store as an array in a new variable
+// 2. Use the reverse method on that array and store that in a new variable 
+// 3. Put the letter back together to show the final word
+
+//function reverse(word) {
+//  var word_arr = word.split('');
+//  var reversed_word = word_arr.reverse();
+//  var reversed_word.join();
+//}
+//
+//console.log(reverse("hello"))
