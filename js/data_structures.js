5d8d2dd254b1abf2b68a14d8e45f8ee9290edf6a
Rakey Drammeh <rakeydrammeh@Rakeys-MBP.fios-router.home>
2017-04-21 12:20:27 -0400

    First attempt at creating an empty object

--- js/data_structures.js
+++ js/data_structures.js
@@ -4,3 +4,17 @@ var names = ["Skippy", "Slappy", "Stinky", "Mule"]
 
 colors.push("pink");
 names.push("Strinky");
+
+
+function newObject {
+  return {};
+}
+
+for (i = 0; i < names.length; i++) {
+  console.log("This horses name is" + " " + names[i] + "," + "and has a color of" + " " + colors[i] + ".");
+  newObject(names[i]) = colors[i];
+}
+
+return newObject();
+
+
