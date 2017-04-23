961b63a89d3874e06f26e899e99804815594c246
Rakey Drammeh <rakeydrammeh@Rakeys-MBP.fios-router.home>
2017-04-23 19:51:09 -0400

    Created another array to test longest phrase function

--- js/algos.js
+++ js/algos.js
@@ -1,13 +1,17 @@
 /*
 PSEUDOCODE FOR RELEASE 0
 1. Create a new variable that takes an array of phrases
-2. Create a function that loops through the array 
-3. Use control flow to determine which word is the longest using .length
+2. Create a function that takes an array as a parameter and loops through the array 
+3. Create a longestWord variable as a comparer to start with 
+4. Use control flow to determine which word is longer than the initial word 
+5. Reset the value of the longestWord variable to include the longest in the array
+6. Return the longestWord variable
 4. Call the function 
 */
 
 
 var myArray = ["long phrase","longest phrase","longer phrase", "longestestestest phrase"];
+var anotherArray = ['peanut butter', 'jelly', 'whole wheat bread'];
 
 function longestPhrase(array) {
   var longestWord = array[0]
@@ -20,3 +24,4 @@ function longestPhrase(array) {
 }
 
 console.log(longestPhrase(myArray));
+console.log(longestPhrase(anotherArray));
