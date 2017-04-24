ad931f67c7d11db0545475386b7721e37b084169
Rakey Drammeh <rakeydrammeh@Rakeys-MBP.fios-router.home>
2017-04-23 20:32:38 -0400

    Created a function for release 1 that compares key-value pairs in two separate objects.

--- js/algos.js
+++ js/algos.js
@@ -1,3 +1,5 @@
+// ---- RELEASE 0 ---- ///
+
 /*
 PSEUDOCODE FOR RELEASE 0
 1. Create a new variable that takes an array of phrases
@@ -8,7 +10,6 @@ PSEUDOCODE FOR RELEASE 0
 6. Return the longestWord variable
 4. Call the function 
 */
-
 
 var myArray = ["long phrase","longest phrase","longer phrase", "longestestestest phrase"];
 var anotherArray = ['peanut butter', 'jelly', 'whole wheat bread'];
@@ -25,3 +26,38 @@ function longestPhrase(array) {
 
 console.log(longestPhrase(myArray));
 console.log(longestPhrase(anotherArray));
+
+// ---- RELEASE 1 ---- ///
+
+/*
+PSEUDOCODE FOR RELEASE 1
+1. Create variables with key-value pairs 
+2. Assign the objects keys into two separate variables 
+3. Create a function that takes both two Objects as a parameter and loops through the Objects keys  
+4. Use control flow to return true if two key value are the same using their index  
+5. Call the function using different examples
+*/
+
+var myInfo = {name: 'Rakey', age: 24};
+var friendInfo = {name: 'Liz', age: 30};
+var randomPerson = {name: 'Sara', age: 24}
+
+function keyValueMatch(array1, array2) {
+  var keys1 = Object.keys(array1);
+  var keys2 = Object.keys(array2);
+  var shareValue = true
+  for (var i = 0; i < keys1.length; i++) {
+      if (array1[keys1[i]] === array2[keys2[i]]) {
+        shareValue = true
+      } else {
+        shareValue = false
+      }
+  }
+  return shareValue
+}
+
+
+console.log(keyValueMatch(myInfo, friendInfo));
+console.log(keyValueMatch(myInfo, randomPerson));
+console.log(keyValueMatch(friendInfo, randomPerson));
+
