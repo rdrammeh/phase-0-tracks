4ca2995199e52a86ca1eb03d536aec9a3a12e5e2
Rakey Drammeh <rakeydrammeh@Rakeys-MBP.fios-router.home>
2017-05-07 18:52:02 -0400

    Module 9.5

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -1,5 +1,6 @@
 # require gems
 require 'sinatra'
+require "sinatra/reloader" if development?
 require 'sqlite3'
 
 set :public_folder, File.dirname(__FILE__) + '/static'
@@ -24,4 +25,25 @@ post '/students' do
   redirect '/'
 end
 
+get '/students/:campus' do
+  query = <<-SQL
+    SELECT * FROM students WHERE LOWER(campus)=LOWER("#{params[:campus]}")
+  SQL
+  @students = db.execute(query)
+  @campus = params[:campus]
+  erb :students
+end 
+
+get '/campus/update' do
+  erb :new_campus
+end
+
+post "/update/campus" do
+  query = <<-SQL
+    UPDATE students SET campus="#{params['campus']}" WHERE LOWER(name)=LOWER("#{params['name']}")
+  SQL
+  db.execute(query)
+  campus = params['campus']
+  redirect "/students/#{campus}"
+end
 # add static resources
